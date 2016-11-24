0ee1fc3687d6789c8b7e5c6e4ec075592e607b2b
linux <linux@linux-ElementaryOS.(none)>
2016-11-24 10:44:59 +0800

    concatenation

---
+++ text03.pl
@@ -0,0 +1,9 @@
+#!/usr/bin/perl -w
+$DNA1 = 'AAAAAAAAAAAGG';
+$DNA2 = 'TTTTTCCCCC';
+print "The original two DNA fragments:\n\n";
+print $DNA1,"\n";
+print $DNA2,"\n\n";
+print "The concatenation of the two fragments is:\n\n";
+print $DNA1,$DNA2,"\n";
+exit;
